7ad73fb14bc52e6f375654d16805cf716ef17b4d
Flávio <flavioandrade0201@gmail.com>
2024-07-17 11:17:14 +0900

    Create arrays.py

---
+++ Python/arrays.py
@@ -0,0 +1,28 @@
+# Linear Arrays
+
+# Multi-dimensional Arrays
+
+# Dynamic Arrays
+
+# Jagged Arrays
+
+# Arrays operations
+
+# size()
+# capacity()
+# is_empty()
+# at(index)
+# push(item)
+# insert(index, item)
+# prepend(item)
+# pop()
+# delete(index)
+# remove(item)
+# find(item)
+# resize(new_capacity)
+
+# Time 
+# O(1) - add/remove at the end
+# O(n) - insert/remove elsewhere
+
+# Space
