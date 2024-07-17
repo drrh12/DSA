94d5a70d94f1728e46923b94010c650c55f2007e
Flávio <flavioandrade0201@gmail.com>
2024-07-17 15:32:40 +0900

    Create linked_list.py

---
+++ Python/linked_list.py
@@ -0,0 +1,12 @@
+# linked list
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.next = None
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
